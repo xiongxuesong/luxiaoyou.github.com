edd7bdf16f0ef610268a679032a33391c03f34d3
luxiaoyou <luxiaoyou@126.com>
2014-12-05 11:33:03 +0800

    博客优化
    博客优化，精简了部分代码

---
+++ javascripts/floatmenu.js
@@ -0,0 +1,11 @@
+		//<![CDATA[ 
+		var name = "#floatMenu"; 
+		var menuYloc = null; 
+		$(document).ready(function() { 
+		menuYloc = parseInt($(name).css("top").substring(0, $(name).css("top").indexOf("px"))) 
+		$(window).scroll(function() { 
+		offset = menuYloc + $(document).scrollTop() + "px"; 
+		$(name).animate({ top: offset }, { duration: 500, queue: false }); 
+		}); 
+		}); 
+		//]]> 
